9a55ceb3eb5e7b86a75d5e64c3d9009e0291c572
rubencg80 <125598881+rubencg80@users.noreply.github.com>
2023-06-07 09:43:21 -0600

    Add files via upload

---
+++ Holamundo.java
@@ -0,0 +1,6 @@
+public class HolaMundo{ 
+     public static void main(String[]args){ /*Esta línea se conoce como método main*/ 
+          System.out.println("¡Hola mundo!"); 
+     }  
+} 
+// Rubén C.G
